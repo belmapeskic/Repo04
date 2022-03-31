dc9b97cf4ff069f1a1d01de2d34bb9d9f835248b
belmapeskic <belma.peskic@gmail.com>
2022-04-01 01:23:53 +0200

    Adding file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var myQuotient;
+    var numOne;
+    var numTwo;
+    myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0)); // assigned 2.5
+console.log(main(16.5, 5.5)); // assigned 3
+console.log(main(102.0, 25.5)); // assigned 4
+module.exports = main;
