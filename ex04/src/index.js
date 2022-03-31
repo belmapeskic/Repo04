5a2370832de1b18d653fe582e9cbb57adad7b672
belmapeskic <belma.peskic@gmail.com>
2022-04-01 00:56:35 +0200

    Adding file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // only change code below this line
+    myDecimal = 8.2;
+    myDecimalE = 0.0000254e+2
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
